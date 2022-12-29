2738dfb251072297e18580a299d8bd13fd0ee931
Osika Martha <68429760+Mataoseeker@users.noreply.github.com>
2022-12-29 06:30:18 +0000

    Added various functionalities to make the app work.

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+import { useState } from 'react'
+export default function App() {
+
+  const [todo, setTodo] = useState ([]);
+  const [newTask, setNewTask] = useState ("")
+
+  const handleChange = (event) => {
+    setNewTask (event.target.value)
+  }
+  const addTask = () => {
+    const task = {
+      id: todo.length === 0 ? 1 : todo[todo.length - 1].id + 1,
+      taskName: newTask,
+    }
+     setTodo ([...todo, task]);
+  };
+
+  const deleteTask = (id) => {
+setTodo (todo.filter((task) => task.id !== id));  
+  };
+
+  return (
+    <div className="App">
+      <h2 className="heading">My To Do List </h2>
+      <div className="inputs">
+        <input type="text" placeholder="Enter your task" onChange={handleChange}/>
+        <button className="button" onClick={addTask}>Add Task</button>
+      </div>
+      <div className="list">
+        {todo.map((task) => {
+          return <div className="block-div">
+             <p className="task-added">{task.taskName}</p> 
+             <button className="second-button" onClick={ () => deleteTask (task.id) }>Delete</button>
+             </div>
+        })}
+      </div>
+    </div>
+  );
+}
